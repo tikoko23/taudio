060c29fefce447312a9d3f4e2311826eb2633936
tikoko23 <code.tikoko@gmail.com>
2026-06-11 10:23:14 +0300

    refactored sample trait methods

--- src/sample.rs
+++ src/sample.rs
@@ -1,3 +1,6 @@
+use crate::Real;
+use std::io::{self, Write};
+
 #[derive(Debug)]
 pub struct Int8;
 
@@ -23,60 +26,60 @@ pub enum SampleType {
 }
 
 mod sealed {
-    use std::io::{self, Write};
+    use crate::sample::{Dyn, Float32, Int8, Int16, Int32};
 
-    use crate::Real;
+    pub trait Sealed: Sized {}
 
-    use super::{Dyn, Float32, Int8, Int16, Int32, SampleType};
+    impl Sealed for Int8 {}
+    impl Sealed for Int16 {}
+    impl Sealed for Int32 {}
+    impl Sealed for Float32 {}
+    impl Sealed for Dyn {}
+}
 
-    pub trait Sealed: Sized {
-        fn write(&self, sample: Real, w: &mut dyn Write) -> io::Result<()>;
-    }
+pub trait Sample: sealed::Sealed {
+    fn write(&self, sample: Real, w: &mut dyn Write) -> io::Result<()>;
+}
 
-    impl Sealed for Int8 {
-        fn write(&self, sample: Real, w: &mut dyn Write) -> io::Result<()> {
-            let s = (sample * 127.0) as i8;
+impl Sample for Int8 {
+    fn write(&self, sample: Real, w: &mut dyn Write) -> io::Result<()> {
+        let s = (sample * 127.0) as i8;
 
-            w.write_all(&s.to_le_bytes())
-        }
-    }
-
-    impl Sealed for Int16 {
-        fn write(&self, sample: Real, w: &mut dyn Write) -> io::Result<()> {
-            let s = (sample * 32767.0) as i16;
-
-            w.write_all(&s.to_le_bytes())
-        }
-    }
-
-    impl Sealed for Int32 {
-        fn write(&self, sample: Real, w: &mut dyn Write) -> io::Result<()> {
-            let s = (sample * 2147483647.0) as i32;
-
-            w.write_all(&s.to_le_bytes())
-        }
-    }
-
-    impl Sealed for Float32 {
-        fn write(&self, sample: Real, w: &mut dyn Write) -> io::Result<()> {
-            let s = sample as f32;
-
-            w.write_all(&s.to_le_bytes())
-        }
-    }
-
-    impl Sealed for Dyn {
-        fn write(&self, sample: Real, w: &mut dyn Write) -> io::Result<()> {
-            match self.0 {
-                SampleType::Int8 => Int8::write(&Int8, sample, w),
-                SampleType::Int16 => Int16::write(&Int16, sample, w),
-                SampleType::Int32 => Int32::write(&Int32, sample, w),
-                SampleType::Float32 => Float32::write(&Float32, sample, w),
-            }
-        }
+        w.write_all(&s.to_le_bytes())
     }
 }
 
-pub trait Sample: sealed::Sealed {}
+impl Sample for Int16 {
+    fn write(&self, sample: Real, w: &mut dyn Write) -> io::Result<()> {
+        let s = (sample * 32767.0) as i16;
 
-impl<T: sealed::Sealed> Sample for T {}
+        w.write_all(&s.to_le_bytes())
+    }
+}
+
+impl Sample for Int32 {
+    fn write(&self, sample: Real, w: &mut dyn Write) -> io::Result<()> {
+        let s = (sample * 2147483647.0) as i32;
+
+        w.write_all(&s.to_le_bytes())
+    }
+}
+
+impl Sample for Float32 {
+    fn write(&self, sample: Real, w: &mut dyn Write) -> io::Result<()> {
+        let s = sample as f32;
+
+        w.write_all(&s.to_le_bytes())
+    }
+}
+
+impl Sample for Dyn {
+    fn write(&self, sample: Real, w: &mut dyn Write) -> io::Result<()> {
+        match self.0 {
+            SampleType::Int8 => Int8::write(&Int8, sample, w),
+            SampleType::Int16 => Int16::write(&Int16, sample, w),
+            SampleType::Int32 => Int32::write(&Int32, sample, w),
+            SampleType::Float32 => Float32::write(&Float32, sample, w),
+        }
+    }
+}
