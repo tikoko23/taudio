809b39780d6f48edc775e0a9a104cc83fd825a4c
tikoko23 <code.tikoko@gmail.com>
2026-06-11 10:24:32 +0300

    updated example to match the new api

--- examples/sine.rs
+++ examples/sine.rs
@@ -1,56 +1,14 @@
-use std::{
-    error::Error,
-    fs::File,
-    io::{BufWriter, Write},
-};
+use std::{error::Error, fs::File, io::BufWriter};
 
+use smallvec::smallvec;
 use taudio::{
-    buffer::SampleChannels,
-    dupe::Dupe,
-    err::AudioError,
-    node::{AudioNode, AudioSink, AudioSinkCfg, AudioSinkInfo, SamplingContext},
     pipeline::PipelineBuilder,
+    sample::{self, Sample},
+    sinks::BufferedSink,
     sources::Osc,
+    wav::{WavChunk, WavFile, WavFormat, WavFormatMeta},
     waveform,
 };
-
-#[derive(Debug)]
-struct FileSink {
-    file: Option<BufWriter<File>>,
-}
-
-impl Dupe for FileSink {}
-
-impl AudioNode for FileSink {
-    fn name(&self) -> &str {
-        "@example:file-sink"
-    }
-}
-
-impl AudioSink for FileSink {
-    fn setup(&mut self, cfg: &AudioSinkCfg) -> Result<AudioSinkInfo, AudioError> {
-        AudioError::expect_channels(1..=1, cfg.num_inputs)?;
-
-        Ok(AudioSinkInfo {})
-    }
-
-    fn sample(
-        &mut self,
-        ctx: &SamplingContext,
-        input: &SampleChannels<'_>,
-    ) -> Result<(), AudioError> {
-        let _ = ctx;
-
-        let chan = input.get_channel(0);
-        let bytes = bytemuck::cast_slice(&chan);
-
-        self.file
-            .as_mut()
-            .unwrap()
-            .write_all(bytes)
-            .map_err(AudioError::boxed)
-    }
-}
 
 fn main() -> Result<(), Box<dyn Error>> {
     let osc = Osc::new(waveform::Sine, 1.0, 1.0, 1);
@@ -58,16 +16,51 @@ fn main() -> Result<(), Box<dyn Error>> {
     let mut builder = PipelineBuilder::default();
 
     let osc_id = builder.add_source(osc)?;
-
-    builder.add_sink(
-        [osc_id.output(0)],
-        FileSink {
-            file: Some(BufWriter::new(File::create("dump.bin")?)),
-        },
-    )?;
+    let sink_id = builder.add_sink([osc_id.output(0)], BufferedSink::new())?;
 
     let mut pipeline = builder.build()?;
     pipeline.sample(44100)?;
+
+    let mut data = None;
+
+    for (id, sink) in pipeline.sinks_mut() {
+        if id != sink_id {
+            continue;
+        }
+
+        if let Some(sink) = sink.downcast_mut::<BufferedSink>() {
+            data = Some(sink.take().next().unwrap());
+        }
+    }
+
+    if let Some(data) = data {
+        let meta = WavFormatMeta {
+            sample_rate: 44100,
+            audio_format: WavFormat::Pcm,
+            num_channels: 1,
+            bits_per_sample: 16,
+        };
+
+        let mut samples = vec![];
+
+        for sample in data {
+            let _ = sample::Int16.write(sample, &mut samples);
+        }
+
+        let data_chunk = WavChunk {
+            id: *b"data",
+            data: samples.into(),
+        };
+
+        let wav = WavFile {
+            chunks: smallvec![WavChunk::new_format(&meta), data_chunk],
+        };
+
+        let file = File::create("sine440hz.wav")?;
+        let mut writer = BufWriter::new(file);
+
+        wav.write(&mut writer)?;
+    }
 
     Ok(())
 }
