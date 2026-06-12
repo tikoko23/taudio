7f8e6d9bdc24541f9783275aa18ea15826ba5901
tikoko23 <code.tikoko@gmail.com>
2026-06-12 09:51:56 +0300

    added more buffer access patterns

--- src/sinks/buffer.rs
+++ src/sinks/buffer.rs
@@ -38,6 +38,12 @@ impl BufferedSink {
         }
     }
 
+    /// Returns the number of channels.
+    #[inline]
+    pub fn num_channels(&self) -> usize {
+        self.buffers.len()
+    }
+
     /// Returns an iterator over the stored samples.
     ///
     /// This function will move the buffers into the iterator. Reuse of the [`BufferedSink`] will
@@ -45,6 +51,56 @@ impl BufferedSink {
     /// if you allocate your own buffers.
     pub fn take(&mut self) -> impl Iterator<Item = Vec<Real>> {
         self.buffers.iter_mut().map(std::mem::take)
+    }
+
+    /// Visits the specified channel, calls the callback with the stored samples and clears the
+    /// buffer.
+    ///
+    /// # Panics
+    /// This function will panic if the given channel index is out of bounds.
+    pub fn visit_channel<F>(&mut self, channel: usize, mut cb: F)
+    where
+        F: FnMut(&[Real]),
+    {
+        cb(&self.buffers[channel]);
+    }
+
+    /// Returns the stored samples of the channel with the given index.
+    ///
+    /// # Panics
+    /// This function will panic if the given channel index is out of bounds.
+    pub fn get_channel(&self, channel: usize) -> &[Real] {
+        &self.buffers[channel]
+    }
+
+    /// Returns the stored samples of the channel with the given index.
+    ///
+    /// # Panics
+    /// This function will panic if the given channel index is out of bounds.
+    pub fn get_channel_mut(&mut self, channel: usize) -> &mut [Real] {
+        &mut self.buffers[channel]
+    }
+
+    /// Returns the stored samples of the channel with the given index.
+    ///
+    /// # Panics
+    /// This function will panic if the given channel index is out of bounds.
+    pub fn try_get_channel(&self, channel: usize) -> Option<&[Real]> {
+        self.buffers.get(channel).map(|c| c.as_slice())
+    }
+
+    /// Returns the stored samples of the channel with the given index.
+    pub fn try_get_channel_mut(&mut self, channel: usize) -> Option<&mut [Real]> {
+        self.buffers.get_mut(channel).map(|c| c.as_mut_slice())
+    }
+
+    /// Returns the stored samples of the channel with the given index and clears the inner buffer.
+    ///
+    /// This function will move the buffer into the return value. Reuse of the [`BufferedSink`] will
+    /// cause new allocations. See [`BufferedSink::get_channel`] for a non-owning, no-alloc alternative
+    /// if you allocate your own buffers.
+    pub fn take_channel(&mut self, channel: usize) -> Vec<Real> {
+        std::mem::take(&mut self.buffers[channel])
     }
 }
 
